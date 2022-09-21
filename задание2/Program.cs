f323eff9f56a9ce8c95a5d1c94cd651061ecc8a0
Liza <elizaveta.b.elizaveta@yandex.by>
2022-09-21 21:05:44 +0300

    задание2

---
+++ задание2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значения M и N. 
+//Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int M = ReadInt ("Введите число M: ");
+int N = ReadInt ("Введите число N: ");
+
+int SumNumbers(int M, int N)
+{
+    if (N > M)
+        return N + SumNumbers(M, N - 1);
+    return M;
+}
+int result = SumNumbers (M, N);
+Console.WriteLine($"Сумма натуральных элементов в промежутке от {M} до {N} равна {result}.");
+
+int ReadInt (string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}

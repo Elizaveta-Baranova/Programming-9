7a0a7ccc99a7336362364297b13eb1cf8b28e1c7
Liza <elizaveta.b.elizaveta@yandex.by>
2022-09-21 21:05:34 +0300

    задание1

---
+++ задание1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте значения N. 
+//Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+
+Console.Write("Введите целое число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count = 2;
+PrintNumber(number, count);
+Console.Write(1);
+
+void PrintNumber(int number, int count)
+{
+  if (count > number) return;
+  PrintNumber(number, count + 1);
+  Console.Write(count + ", ");
+}
